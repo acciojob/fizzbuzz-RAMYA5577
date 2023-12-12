f5577094e0e4e9540914ce9135cbcb19280aff4a
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-12 09:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let n=20;
+let array=[];
+for(let i=1;i<=n;i++){
+	if(i%3==0 && i%5==0){
+		array.push("FizzBuzz");
+	}
+	else if(i%3==0){
+		array.push("Fizz");
+	}
+	else if(i%5==0){
+		array.push("Buzz");
+	}
+	else
+		array.push(i);
+}
+alert(array);
